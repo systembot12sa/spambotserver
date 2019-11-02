2b79c315261e77bd2abe0167943a420d7785183a
systembot12sa <56450830+systembot12sa@users.noreply.github.com>
2019-11-02 19:28:03 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['541002506084286467'];// ايدي حسابك
-const prefix = ['%'];
-
+const prefix = ['-'];
+ 
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : n3k4a `);
@@ -15,33 +15,33 @@ client.on('ready', () => {
    console.log(`This Bots Online ' `);
    console.log(`----------------`);
 });
-
+ 
 client2.on('ready', () => {
    console.log(`----------------`);
    console.log(`2 Account Online ✩' `);
    console.log(`----------------`);
 });
-
-
-
+ 
+ 
+ 
 client.on('message', message => {
     if(message.content === prefix+'راتب'){
         message.channel.send('#daily')
     }
 });
-
+ 
 client.on('message', message => {
     if(message.content === prefix+'مبلغ'){
         message.channel.send('#credits')
     }
 });
-
+ 
 client.on('message', message => {
     if(message.content === prefix+'ريب'){
         message.channel.send("#rep "+"<@" + myid + ">")
     }
 });
-
+ 
 client.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
@@ -51,22 +51,22 @@ if (message.content === prefix+'spam') {
           .then(m => {
             count++;
           })
-          
+         
         }
       }
 });
-
+ 
 client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-
+ 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
-
+ 
   let args = message.content.split(" ").slice(1);
-
-
-
+ 
+ 
+ 
 if (command == "تحدث") {
 let rank = message.guild.member(message.author).roles.find('name', 'Role.n3k4a'); //لازم تعمل الرتبه دي في السيرفر  Role.n3k4a
 if (!rank) return message.reply(' ')
@@ -74,25 +74,25 @@ if (!rank) return message.reply(' ')
     message.delete();
   }
 });
-
+ 
 client2.on('message', message => {
     if(message.content === prefix+'راتب'){
         message.channel.send('#daily')
     }
 });
-
+ 
 client2.on('message', message => {
     if(message.content === prefix+'مبلغ'){
         message.channel.send('#credits')
     }
 });
-
+ 
 client2.on('message', message => {
     if(message.content === prefix+'ريب'){
-        message.channel.send("#rep "+"<@" + myid + ">") 
+        message.channel.send("#rep "+"<@" + myid + ">")
     }
 });
-
+ 
 client2.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
@@ -102,22 +102,22 @@ if (message.content === prefix+'spam') {
           .then(m => {
             count++;
           })
-          
+         
         }
       }
 });
-
+ 
 client2.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-
+ 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
-
+ 
   let args = message.content.split(" ").slice(1);
-
-
-
+ 
+ 
+ 
 if (command == "تحدث") {
 let rank = message.guild.member(message.author).roles.find('name', 'Role.n3k4a');
 if (!rank) return message.reply(' ')
@@ -125,7 +125,7 @@ if (!rank) return message.reply(' ')
     message.delete();
   }
 });
-
-
+ 
+ 
 client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
